b8dc97a124ce791732d24b275e2de7d4c22437de
John Carmichael <john@johnine.org>
2021-01-16 17:28:07 +0100

    query and hash contents now accept pretty much anything

--- build/index.js
+++ build/index.js
@@ -1,7 +1,20 @@
 'use strict';
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = (input) => {
-    const regex = /((?<SCHEME>(?:https))(?<HOSTNAME>\:\/\/(?:www.|[a-zA-ZÀ-ž.]+)[a-zA-ZÀ-ž0-9\-\.]+\.(?:[\w]{1,}))?(?<PORT>\:[0-9][0-9]{0,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])?(?<PATH>[a-zA-ZÀ-ž0-9\-\.\/\+]+)?(?<QUERY>(?:\?$|[a-zA-ZÀ-ž0-9\.\,\;\?\'\\\+&%\$\=~_\-\*]+))?(?<HASH>(?:#[a-zA-ZÀ-ž0-9\.\,\;\?\'\\\+&%\$\=~_\-\*]+))?)/gim;
+    const scheme = '(?:https)';
+    const hostname = '(\\:\\/\\/(?:www.|[a-zA-ZÀ-ž.]+)[a-zA-ZÀ-ž0-9\\-\\.]+\\.(?:[\\w]{1,}))?';
+    const port = '(\:[0-9][0-9]{0,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])?';
+    const path = '([a-zA-ZÀ-ž0-9\\-\\.\\/\\+]+)?';
+    const query = '(?:\\?$|[^\\s"]*)?';
+    const hash = '(?:#[^\\s"]*)?';
+    // As named groups for when IE finally goes away:
+    // const scheme = '(?<SCHEME>(?:https))';
+    // const hostname = '(?<HOSTNAME>\\:\\/\\/(?:www.|[a-zA-ZÀ-ž.]+)[a-zA-ZÀ-ž0-9\\-\\.]+\\.(?:[\\w]{1,}))?';
+    // const port = '(?<PORT>\:[0-9][0-9]{0,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])?'
+    // const path = '(?<PATH>[a-zA-ZÀ-ž0-9\\-\\.\\/\\+]+)?';
+    // const query = '(?<QUERY>(?:\\?$|[^\\s"]*)?)?';
+    // const hash = '(?<HASH>(?:#[^\\s"]*)?)?';
+    const regex = new RegExp(`(${scheme}${hostname}${port}${path}${query}${hash})`, 'gim');
     const out = regex.exec(input);
     if (out) {
         return out[0];
--- src/__tests__/getFirstUrlFromString.spec.ts
+++ src/__tests__/getFirstUrlFromString.spec.ts
@@ -38,7 +38,9 @@ test('should return a long complex urls', () => {
   const urls = [
     'https://www.booking.com/hotel/pt/aldeamento-villas-mourim-by-garvetur.de.html?aid=304142;label=gen173nr-1DCAEoggI46AdIM1gEaDuIAQGYAQe4AQfIAQzYAQPoAQGIAgGoAgO4AvjN-PoFwAIB0gIkODEwNGMzYmYtNWEzYi00YmY2LWIxZDYtZDZiNzg2NjIxYzg52AIE4AIB;sid=78f413d8d370558b3dad6bb7e0e8ca41;all_sr_blocks=189010603_199461783_0_0_0;checkin=2020-09-21;checkout=2020-09-30;dest_id=171;dest_type=country;dist=0;group_adults=2;group_children=0;hapos=1;highlighted_blocks=189010603_199461783_0_0_0;hpos=1;no_rooms=1;room1=A%2CA;sb_price_type=total;sr_order=popularity;sr_pri_blocks=189010603_199461783_0_0_0__80000;srepoch=1600006554;srpvid=d114644c15bd017d;type=total;ucfs=1&#map_closed=ä',
     'https://www.real.de/product/338661983/?kwd=&source=pläa&sid=21915679&gclid=Cj0KCQiArvX_BRCyARIsAKsnTxMs1vcxfSKIoA2UEG5L428h_F99p5H1ww9gmaKbTcLh9-pJKKJSCyAaArxDEALw_wcB',
-    'https://www.amazon.de/-/en/Throw-Burrito-Exploding-Kittens-Dodgeball/dp/B07TS96J7Q/?_encoding=UTF8&pd_rd_w=U1Chq&pf_rd_p=e1c506bc-20f2-4103-883d-bfc914d48327&pf_rd_r=JHCKJP8X0NS835A7XT10&pd_rd_r=807eab37-eb12-4994-8a67-4ea9ä82dbde00&pd_rd_wg=MYTRk&ref_=pd_gw_unk'
+    'https://www.amazon.de/-/en/Throw-Burrito-Exploding-Kittens-Dodgeball/dp/B07TS96J7Q/?_encoding=UTF8&pd_rd_w=U1Chq&pf_rd_p=e1c506bc-20f2-4103-883d-bfc914d48327&pf_rd_r=JHCKJP8X0NS835A7XT10&pd_rd_r=807eab37-eb12-4994-8a67-4ea9ä82dbde00&pd_rd_wg=MYTRk&ref_=pd_gw_unk',
+    'https://shop.shop.com/some/shoes#/something/page',
+    'https://shop.shop.com/some/shoes?/something/page#1=/hello-world!%$',
   ];
   urls.forEach((url: string) => {
     expect(getFirstUrlFromString(` sergdgf  ${url} sdliushvlsdiuh lsdn`)).toBe(url);
